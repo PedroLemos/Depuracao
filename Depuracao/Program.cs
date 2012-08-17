8d6f057baa882e7929427b4cc7f37f37bdde54de
Pedro Hugo <pedro__hugo__@hotmail.com>
2012-08-16 21:34:42 -0300

    Commitada sucundaria

--- Depuracao/Program.cs
+++ Depuracao/Program.cs
@@ -11,6 +11,12 @@ namespace Depuracao
             n = n + 1;
             return n;
         }
+
+        static float Dividir(int n, int d)
+        {
+            return n / d;
+        }
+
         static void Main(string[] args)
         {
             int numero = 0;
@@ -18,6 +24,13 @@ namespace Depuracao
             Console.WriteLine("O numero escolhido é: {0}", numero);
             numero = Mudar(numero);
             Console.WriteLine("O numero modificado é: {0}", numero);
+
+            int dominador = 0;
+            dominador = int.Parse(Console.ReadLine());
+            if (dominador == 0)
+                Console.WriteLine("Não é possivel dividir {0} por zero", numero);
+            else
+                Console.WriteLine("O quociente entre {0} e {1} é {2}", numero, dominador, Dividir(numero, dominador));
             Console.Read();
         }
     }
